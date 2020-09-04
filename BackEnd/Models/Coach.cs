57b6f1cb6550f9041746744275b7e70cf8fcd6ad
tookie bunten <davidtookiebunten@gmail.com>
2020-09-04 15:29:19 +0100

    Started to add the DTO components under netstandard2.0

--- BackEnd/Models/Coach.cs
+++ BackEnd/Models/Coach.cs
@@ -1,27 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BackEnd.Models
+﻿namespace BackEnd.Models
 {
-    public class Coach
+    public class Coach : EventsDTO.Coach
     {
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        public string FirstName { get; set; }
-
-        [Required]
-        [StringLength(50)]
-        public string LastName { get; set; }
-
-        [StringLength(200)]
-        public string UserName { get; set; }
-
-        [StringLength(2000)]
-        public string CoachDetails { get; set; }
+       
     }
 }
---
+++ EventsDTO/Coach.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventsDTO
+{
+    public class Coach
+    {
+        public int Id { get; set; }
+        //Coach's first name
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        //Coach's last name
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        //username or full name of coach
+        [StringLength(200)]
+        public string UserName { get; set; }
+        //Details of the coach
+        [StringLength(2000)]
+        public string CoachDetails { get; set; }
+    }
+}
