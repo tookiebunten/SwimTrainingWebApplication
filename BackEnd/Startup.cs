c9f28cf3dae116213fc5eace152187e890b42455
tookie bunten <davidtookiebunten@gmail.com>
2020-09-04 14:48:07 +0100

    Added swagger to expose and test endpoints

--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Threading.Tasks;
 
 namespace BackEnd
 {
@@ -19,13 +21,21 @@ namespace BackEnd
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
+
         {
+            //Adding Sqlite connection
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlite("Data Source =events.db");
             });
 
             services.AddControllers();
+
+            //Adding Swashbuckler services
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Conference Planner API", Version = "v1" }
+                )
+               );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,12 +48,23 @@ namespace BackEnd
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Conference Planner API v1")
+            );
+
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/", context => {
+                    context.Response.Redirect("/swagger/");
+                    return Task.CompletedTask;
+                });
+
                 endpoints.MapControllers();
             });
         }
