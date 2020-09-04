7a1388f88da1629057ed2c58016f36129ae437cd
tookie bunten <davidtookiebunten@gmail.com>
2020-09-04 10:21:36 +0100

    Pushing my first changes

--- BackEnd/Models/Coaches.cs
+++ BackEnd/Models/Coaches.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace BackEnd.Models
 {
     public class Coaches
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(200)]
+        public string UserName { get; set; }
+
+        [StringLength(2000)]
+        public string CoachDetails { get; set; }
     }
 }
