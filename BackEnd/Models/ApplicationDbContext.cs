9c82a27845790b182522a162f715f66692500065
tookie bunten <davidtookiebunten@gmail.com>
2020-09-04 12:46:09 +0100

    Added ApplicationDbContext to Models

---
+++ BackEnd/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+            public DbSet<Coaches> Coaches { get; set; }
+    }
+    
+}
