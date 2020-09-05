a907864f7390da358a59163396121e99ee33648f
tookie bunten <davidtookiebunten@gmail.com>
2020-09-05 08:01:16 +0100

    Created Swimmers DTO

---
+++ EventsDTO/Swimmers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EventsDTO
+{
+    namespace EventsDTO
+    {
+        public class Swimmers
+        {
+            public int Id { get; set; }
+            //Swimmer's first name
+            [Required]
+            [StringLength(50)]
+            public string FirstName { get; set; }
+            //Swimmer's last name
+            [Required]
+            [StringLength(50)]
+            public string LastName { get; set; }
+            //username or full name of Swimmer
+            [StringLength(200)]
+            public string UserName { get; set; }
+            //Swimmer's email address
+            [StringLength(100)]
+            public string EmailAddress { get; set; }
+        }
+    }
+}
